bbf41c72145066502a2aeb260dbeb69815db7042
Jorge Guerra <jorguerra@gmail.com>
2019-09-07 12:13:36 -0400

    Seeders de posts y comentarios

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Post.destroy_all
+10.times do |i|
+  user = User.create(name: "User#{i}", email: "email#{i}@mail.com", password: "123456")
+  post = Post.create(user: user, title: "post #{i}", content: "content #{i}")
+  10.times do |x|
+    comment = post.comments.build(content: "comment #{x}", post: post, user: user)
+    comment.save!
+  end
+end
